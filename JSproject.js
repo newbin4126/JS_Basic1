f4694625a0a69260fac02b93c846fcf90a9e1c52
newbin4126 <dkfmal710@gmail.com>
2023-12-28 23:39:59 +0900

    JSstudy-Day2최유빈

--- JSproject.js
+++ JSproject.js
@@ -1,1 +1,37 @@
-alert("hi");
+/*alert("hi");
+
+console.log();//콘솔에 메세지보내기 숫자나 string넣기 가능
+
+const a=5;
+const b=2;
+const myName ='newbin';
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello "+myName);
+
+const amIFat = null;
+let something;
+console.log(amIFat);*/
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const dayOfWeek = [mon,tue,wed,thu,fri,sat,sun];
+
+const nonsense=[1,2,"hello",false,null,undefined,"new"];
+
+
+
+console.log(dayOfWeek[4]);//array에서 아이템 받아오기
+
+dayOfWeek.push("sun");//array안에 요일 더 추가하기
+
+console.log(dayOfWeek);
+
